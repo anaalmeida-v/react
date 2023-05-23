6a566255164296b2738bbab208f94e9001be5e26
ana <blabla@exemplo.br>
2023-05-23 17:46:43 -0300

    Comentários nos componentes

--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,15 @@
+//arquivo de estilo
+
 const FirstComponent = () => {
+    /*
+    bla
+    bla
+    mult
+    line
+    */
     return (
         <div>
+            {/*algum coment*/}
             <h1>Meu primeiro componente</h1>
             <p className="teste">Meu texto</p>
         </div>
