dba051072aa262c45f901057bab523f98bd746ee
ana <blabla@exemplo.br>
2023-05-23 17:29:40 -0300

    JSX

--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -2,7 +2,9 @@ const FirstComponent = () => {
     return (
         <div>
             <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
         </div>
+        //os elementos precisam ser encapsulados em uma única tag
     )
 }
 
