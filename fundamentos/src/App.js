090bd1df05e8ef79d830b441d6074ae9049571c4
ana <blabla@exemplo.br>
2023-05-23 17:10:53 -0300

    importacao de elemento

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,9 +1,14 @@
+//components
+import FirstComponent from './components/FirstComponent';
+//importação de FirstComponent para o arquivo JS FirstComponent
+//styles / CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent />
     </div>
   );
 }
