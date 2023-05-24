e54565f3abc33b115ba2aeaa53988eedcc8c8987
ana <blabla@exemplo.br>
2023-05-24 17:02:35 -0300

    hooks

--- avancando_em_react/src/components/ManageData.js
+++ avancando_em_react/src/components/ManageData.js
@@ -1,27 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const ManageData = () => {
-    let someData = 10
+  const someData = 10;
 
-    const [number, setNumber] = useState(15)
+  const [number, setNumber] = useState(15);
 
-    console.log(number)
-    return (
-        <div>
-            <p>Valor: {someData}</p>
-            <button onClick={() => {
-                someData = 15
-            }}>Mudar variável</button>
-        </div>
-        <div>
-            <p>Valor: {number}</p>
-            <button onClick={() => {
-                setNumber(25)
-            }}>
-                Mudar o State 
-            </button>
-        </div>
-    )
-}
+  console.log(number)
+  return (
+    <div>
+      <div>
+        <p>Valor: {someData}</p>
+        <button onClick={() => (someData = 15)}>Mudar variável</button>
+      </div>
+      <div>
+        <p>Valor: {number}</p>
+        <button onClick={() => number(20)}>Mudar state</button>
+      </div>
+    </div>
+  );
+};
 
-export default ManageData
+export default ManageData;
