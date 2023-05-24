0cacc0a626b2a31ed7ed97a7c9c5d14ebc107cee
ana <blabla@exemplo.br>
2023-05-24 14:40:26 -0300

    funcao de renderizacao

--- fundamentos/src/components/Events.js
+++ fundamentos/src/components/Events.js
@@ -5,6 +5,14 @@ const Events = () => {
 
         console.log("Ativou o evento!")
     }
+
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso!</h1>
+        } else {
+            return <h1>Também posso renderizar isso!</h1>
+        }
+    }
     return(
         <div>
             <div>
@@ -23,6 +31,9 @@ const Events = () => {
                 }}>Clique aqui, por favor.</button>
             </div>
             {/*Pode-se perceber que nesse caso, uma função distinta funcionaria melhor*/}
+
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     )
 }
