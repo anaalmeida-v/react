26bdfac615ce049c4a9220dfbf6132f19de0d848
ana <blabla@exemplo.br>
2023-05-26 15:39:06 -0300

    inline style dinamico

--- css_react/css/src/App.js
+++ css_react/css/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import {useState} from "react";
 
 function App() {
+  const n = 15
+  const [name] = useState("Aninha")
+
   return (
     <div className="App">
       {/* CSS Global */}
@@ -13,6 +17,17 @@ function App() {
       <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>Este elemento foi estilizado de forma inline</p>
       {/* no style acima, a primeira chave aberta serve para adicionar algo dinâmico e a segunda
       serve para adicionar um objeto */}
+      {/* CSS Inline Dinâmico */}
+      <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS Dinâmico</h2>
+      <h2 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS Dinâmico</h2>
+
+      <h2 style={
+        name == "Aninha"
+          ? { color: "green", backgroundColor: "#000" }
+          : null
+      }
+      >
+        Teste nome</h2>
     </div>
   );
 }
