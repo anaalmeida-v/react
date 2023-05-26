ed428f4411cebcf5d06f34948a4cda0433eb2f75
ana <blabla@exemplo.br>
2023-05-26 15:06:57 -0300

    CSS de componentes

--- css_react/css/src/App.js
+++ css_react/css/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1> React com CSS</h1>
+      {/* CSS de Componente */}
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
+      
     </div>
   );
 }
