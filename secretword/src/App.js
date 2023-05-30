5538fa86d537f07c4687bfda77d4a3760e2dab5f
ana <blabla@exemplo.br>
2023-05-30 11:26:29 -0300

    Consumindo states do jogo

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -27,6 +27,11 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("")
   const [letters, setLetters] = useState([])
 
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
+
   const pickWordAndCategory = () => {
     //pick a random category 
     const categories = Object.keys(words)
@@ -51,7 +56,7 @@ function App() {
 
     //create an array of letters 
     let wordLetters = word.split("")
-    {/* separa as letras da palavra */}
+    {/* separa as letras da palavra */ }
 
     console.log(word, category)
     console.log(wordLetters)
@@ -62,7 +67,7 @@ function App() {
     //fill states
     setPickedWord(word)
     setPickedCategory(category)
-    setLetters(letters)
+    setLetters(wordLetters)
 
     setGameStage(stages[1].name)
   }
@@ -80,7 +85,17 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && 
+      <Game 
+        verifyLetter={verifyLetter}
+        pickedWord={pickedWord}
+        pickedCategory={pickedCategory}
+        letters={letters}
+        guessedLetters={guessedLetters}
+        wrongLetters={wrongLetters}
+        guesses={guesses}
+        score={score}
+        />}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
--- secretword/src/components/Game.js
+++ secretword/src/components/Game.js
@@ -1,28 +1,35 @@
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
   return (
     <div className='game'>
       <p className='points'>
-      <span>Pontuação: 000</span>
+        <span>Pontuação: 000</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
-      <h3 className='tip'>Dica sobre a palavra: <span>Dica...</span></h3>
+      <h3 className='tip'>Dica sobre a palavra: <span>{pickedCategory}</span></h3>
+      <p>Você ainda tem {guesses} tentativas(s).</p>
       <div className="wordContainer">
-        <span className="letter">A</span>
-        <span className="blankSquare"></span>
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter)/*se a letra já foi adivinhada*/
+            ? (<span key={i} className='letter'>{letter}</span>//retornar letra
+            ) : (
+              <span key={i} className='blankSquare'></span>//se não, retornar quadro branco
+            )
+        ))}
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
         <form>
-          <input type="text" name='letter' maxLength="1" required/>
+          <input type="text" name='letter' maxLength="1" required />
           <button>Jogar!</button>
         </form>
       </div>
       <div className="wondLettersContainer">
         <p>Letras já utilizadas:</p>
-        <span>a, </span>
-        <span>b, </span>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
       </div>
     </div>
   )
