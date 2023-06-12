3248cffc30d1c56eeac4076e19833deb04f5a9aa
ana <blabla@exemplo.br>
2023-06-12 18:19:57 -0300

    Carregando dados +  Rotas dinâmicas

--- reactrouter/react-router/src/App.js
+++ reactrouter/react-router/src/App.js
@@ -5,24 +5,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 
 //pages
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+      {/*2 - links com react router*/}
         <Navbar />{/*se repete entre as rotas, mas tem elementos no React Router*/}
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        {/* 4 - rota dinâmica */}
+        <Route path="/products/:id" element={<Product />}></Route>{/* linkando produtos a uma rota dinâmica*/}
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
 
 //elementos fora do routes, estarão sendo exibidos em todas as telas
--- reactrouter/react-router/src/pages/Home.js
+++ reactrouter/react-router/src/pages/Home.js
@@ -1,6 +1,31 @@
+import { Link } from 'react-router-dom'
+import { useFetch } from "../hooks/useFetch"
+
+import "./Home.css"
+
 const Home = () => {
+  //3 - carregamento de dados
+  const url = "http://localhost:3000/products"
+
+  const { data: items, loading, error } = useFetch(url)
+  //chamando propriedados vindas do hook //data - dados
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Produtos</h1>
+      {error && <p>{error}</p>}{/* se houver erro, será imprimido o mesmo */}
+      <ul className='products'>
+        {items && items.map((item) => (//checando se itens chegaram, se sim, será feito um .map em cada item(esse .map chama-se 'item')
+          <li key={item.id}>
+            <h2>{item.name}</h2>
+            <p>R$: {item.price}</p>
+            {/* 4 - rota dinâmica */}
+            <Link to={`/products/${item.id}`}>Detalhes</Link>{/* um link q leva aos detalhes do produto */}
+            {/* link para item dinamico(por isso fica entre chaves) */}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
