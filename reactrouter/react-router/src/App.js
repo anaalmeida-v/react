5c754394ad01ee26eefc82ae20e624037cbe187f
ana <blabla@exemplo.br>
2023-06-12 15:02:51 -0300

    configurando o react router

--- reactrouter/react-router/src/App.js
+++ reactrouter/react-router/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+//1 - config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+//elementos fora do routes, estarão sendo exibidos em todas as telas
