51dab4dcf4449fa7b799327efa510b8db49d1bb4
ana <blabla@exemplo.br>
2023-06-12 15:21:49 -0300

    navegação entre as páginas

--- reactrouter/react-router/src/App.js
+++ reactrouter/react-router/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 //1 - config react router
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 //pages
 import Home from './pages/Home';
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />{/*se repete entre as rotas, mas tem elementos no React Router*/}
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
