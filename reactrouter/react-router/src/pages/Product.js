1b4564d1a43219199ee39cb7db699fa93b2d0920
ana <blabla@exemplo.br>
2023-06-13 08:54:10 -0300

    Carregamento dinamico de dados

--- reactrouter/react-router/src/pages/Product.js
+++ reactrouter/react-router/src/pages/Product.js
@@ -1,11 +1,27 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
 const Product = () => {
   //4 - rota dinamica
-
   const {id} = useParams()
+
+  //5 - carregamento dado individual
+  const url = "http://localhost:3000/products/" + id
+
+  const { data: product, loading, error } = useFetch(url)//dados vao vir do useFetch dentro de hooks trazendo uma url
+
+  console.log(product)
   return (
     <>
-    <p>ID do produto: {id}</p>{/*  */}
+    <p>ID do produto: {id}</p>{/* descricao aparecerá na tela de descricao no do produto, parte dinamica feita na Home.js e vinculada com o id e o useParams */}
+    {error && <p>Ocorreu um erro</p>}{/* <- validação */}
+    {loading && <p>Carregando...</p>}{/* <- validação */}
+    {product && (
+      <div>
+        <h1>{product.name}</h1>
+        <p>R${product.price}</p>
+      </div>//exibindo dados do produto
+    )}
     </>
   )
 }
