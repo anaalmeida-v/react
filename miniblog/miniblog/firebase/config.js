fdc72daa91846cc2f42f4ee37331c4c21fc9e18c
ana <blabla@exemplo.br>
2023-06-15 14:44:07 -0300

    integrando firebase ao react

---
+++ miniblog/miniblog/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuIYGc9ooV-FasKM4njRIEBLj_SjXLGgU",
+  authDomain: "miniblog-b38b9.firebaseapp.com",
+  projectId: "miniblog-b38b9",
+  storageBucket: "miniblog-b38b9.appspot.com",
+  messagingSenderId: "1039702486542",
+  appId: "1:1039702486542:web:859f4383d5791a8997965f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)//Firesore - serviço de banco de dados dentro do firebase
+
+export{db}
