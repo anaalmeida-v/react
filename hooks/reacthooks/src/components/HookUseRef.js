5ea68ed129f2007311ef92cde7605a682cfe4a53
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-06-27 18:03:38 -0300

    useRef e DOM

--- hooks/reacthooks/src/components/HookUseRef.js
+++ hooks/reacthooks/src/components/HookUseRef.js
@@ -6,18 +6,46 @@ const HookUseRef = () => {
   const [counter, setCounter] = useState(0);
   const [counterB, setCounterB] = useState(0);
 
-  useEffect(()=>{
-    numberRef.current = numberRef.current + 1
-  })//caso não fosse um Ref o component estaria em um loop infinito
+  useEffect(() => {
+    numberRef.current = numberRef.current + 1;
+  }); //caso não fosse um Ref o component estaria em um loop infinito
+
+  // 2 - useRef e DOM
+  const inputRef = useRef();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setText("");
+
+    inputRef.current.focus();
+  };
 
   return (
     <div>
       <h2>useRef</h2>
+      {/* 1 - useRef */}
       <p>O componente rederizou: {numberRef.current} vezes.</p>
       <p>Counter: {counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>Contador A: {counter}</button>
+      <button onClick={() => setCounter(counter + 1)}>
+        Contador A: {counter}
+      </button>
       <p>Counter: {counterB}</p>
-      <button onClick={() => setCounterB(counterB + 1)}>Contador B: {counterB}</button>
+      <button onClick={() => setCounterB(counterB + 1)}>
+        Contador B: {counterB}
+      </button>
+      <hr />
+      {/* 2 - useRef e DOM */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
   );
 };
