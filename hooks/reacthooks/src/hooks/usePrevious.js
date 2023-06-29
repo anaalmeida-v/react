5c9d6ec7c31efc12c3e457d1ca1ebcf890e6eb14
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-06-29 10:55:27 -0300

    useDebugValue

--- hooks/reacthooks/src/hooks/usePrevious.js
+++ hooks/reacthooks/src/hooks/usePrevious.js
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useDebugValue } from "react"
 
 export const usePrevious = (value) => {//exportação de uma constante(função do hook)- recebe valor(value): valor que existe antes da mudança do mesmo no useState
   const ref = useRef()
+
+  useDebugValue("--- custom hook e usedebugvalue ---")//para execução basta colocar o hook e uma string
+  useDebugValue("o número anterior é: " + value)
 
   useEffect(() => {//utilizado sempre que o hook for solicitado
     ref.current = value
