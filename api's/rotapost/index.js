baa647cdd4d2420875e8a2addbe0cf762ed0edb9
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-06-29 15:48:45 -0300

    Criando rota de POST

---
+++ api's/rotapost/index.js
@@ -0,0 +1,28 @@
+const express = require("express"); //chamando express
+const app = express(); //inicializando express
+
+//ler requisição em json
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json()); //lendo em json
+
+//rotas - endpoints
+app.post('/createproduct', (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  console.log(name);
+  console.log(price);
+
+  res.json({message: `O produto ${name} foi criado com sucesso!`})
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Primeira rota criada com sucesso" }); //resposta em json
+});
+
+app.listen(3000); //executar alguma porta(3000)
