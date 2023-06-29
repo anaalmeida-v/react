7e8a03004893fda3a26a27b140d7c6368264767e
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-06-29 17:33:40 -0300

    Configurando o dotenv

--- reactgram/backend/app.js
+++ reactgram/backend/app.js
@@ -1,15 +1,17 @@
-const express = require("express"); //chamando express
-const path = require("path"); //chamando path - do próprio node.js
-const cors = require("cors"); //para acessar o projeto da própria aplicação de front-end
+require("dotenv").config()//nos da acesso ao arquivo '.env' onde estão as variáveis de ambiente
 
-const port = 5000;
+const express = require("express") //chamando express
+const path = require("path") //chamando path - do próprio node.js
+const cors = require("cors") //para acessar o projeto da própria aplicação de front-end
 
-const app = express(); //inicializando aplicação
+const port = process.env.PORT//inicializando dotenv
+
+const app = express() //inicializando aplicação
 
 //config JSON and form data response
-app.use(express.json);
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json)
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`);
-});
+  console.log(`App rodando na porta ${port}`)
+})
