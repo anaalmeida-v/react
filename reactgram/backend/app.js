c20f77b63a112a4f2b22fdb2a9a83dc39b0061a8
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-06-30 14:03:11 -0300

    Conexão com o banco

--- reactgram/backend/app.js
+++ reactgram/backend/app.js
@@ -12,6 +12,15 @@ const app = express(); //inicializando aplicação
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Solve CORS - quando as requisições são executadas pelo mesmo domínio
+app.use(cors({ credentials: true, origin: "http://localhost:3000" })); //origin - de onde esta vindo a requisição
+
+//Upload diretório - diretório de upload de imagens
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))//juntando nome do diretório atual(_dirname) com /uploads
+
+//DB connection
+require("./config/db.js")
+
 // routes
 const router = require("./routes/Router.js");
 app.use(router); //todas as rotas colocadas em routes>Router.js, servirão como base para nossa aplicação
---
+++ reactgram/backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose"); //importando mongoose
+//connection
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+//trazendo de '.env'
+
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(//conectando ao banco de dados
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster.fvnirbj.mongodb.net/`
+    );//concatenando variáveis q contém user e senha do mongodbproj
+
+    console.log("Coonectou ao banco!")
+    return dbConn;
+  } catch (error) {//caso de erro na conexão
+    console.log(error);
+  }
+};
+
+conn();
+module.exports=conn
