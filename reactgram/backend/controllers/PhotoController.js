f67e969a2b2988b0cfdf6d35c1a47530637133bf
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-04 17:42:31 -0300

    Excluíndo fotos

--- reactgram/backend/controllers/PhotoController.js
+++ reactgram/backend/controllers/PhotoController.js
@@ -22,11 +22,42 @@ const insertPhoto = async (req, res) => {
     //If photo was created successfully, return data - Se a foto foi criada com sucesso, retorne os dados
     if (!newPhoto) {
         res.status(422).json({ errors: ["Houve um problema, por favor tente novamente mais tarde."] })
+        return
     }
 
     res.status(201).json(newPhoto)
 }
 
+//Remove a photo from DB - Remover uma foto do BD
+const deletePhoto = async (req, res) => {
+
+    const { id } = req.params
+    const reqUser = req.user
+
+    try {
+        const photo = await Photo.findById(id)
+
+        //Check if photo exists - Verificando se a foto existe
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada!"] })
+            return
+        }
+
+        //Check if photo belongs to user - Verificando se a foto pertence ao usuário
+        if (!photo.userId.equals(reqUser._id)) {
+            res.status(422).json({ errors: ["Ocorreu um erro, por favor tente novamente mais tarde."] })
+        }
+
+        await Photo.findByIdAndDelete(photo._id)
+
+        res.status(200).json({ id: photo._id, message: "Foto excluída com sucesso!" })
+    } catch (error) {
+        res.status(404).json({ errors: ["Foto não encontrada!"] })
+        return
+    }
+}
+
 module.exports = {
     insertPhoto,
+    deletePhoto
 }
