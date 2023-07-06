5cda47b8383db00cf6aeb6d5356ab6b674a1e2a5
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-06 16:51:23 -0300

    slice for authentication

--- reactgram/frontend/src/store.js
+++ reactgram/frontend/src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'//importação necessária
 
+import authReducer from "./slices/authSlice"
+
 export const store = configureStore({//onde contexts estão sendo salvos(deve ser feita a execução da função importada acima 'configureStore')
     reducer:{//onde fica os contexts
-        
+        auth: authReducer,
     }
 })
