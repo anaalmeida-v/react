3aff59df535998f0adcd6d7d5dedc7f722662e3c
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-06 11:33:53 -0300

    Implementando Redux

--- reactgram/frontend/src/index.js
+++ reactgram/frontend/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Redux
+import { Provider } from 'react-redux'//similar a contextAPI
+//arquivo único que importa os contexts mais variados, só é trabalho um arquivo desse(store.js)
+import { store } from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>{/*o que será entregue aos componentes, tudo oque está no redux*/}
+      <App />
+    </Provider>
   </React.StrictMode>
 )
---
+++ reactgram/frontend/src/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'//importação necessária
+
+export const store = configureStore({//onde contexts estão sendo salvos(deve ser feita a execução da função importada acima 'configureStore')
+    reducer:{//onde fica os contexts
+        
+    }
+})
