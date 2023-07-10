3de78f0c63c0c22aa5c2eddff36d647ab06bbaba
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-10 15:57:30 -0300

    Slice de perfil de usuário

--- reactgram/frontend/src/slices/userSlice.js
+++ reactgram/frontend/src/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import userService from '../services/userService'
 
 const initialState = {//estado inicial com user vazio, mensage é nula
     user: {},
@@ -8,16 +9,38 @@ const initialState = {//estado inicial com user vazio, mensage é nula
     message: null
 }
 
-//funcoes
-export const userSlice = createSlice({//reducer (ficará no store)
+//Get user details
+export const profile = createAsyncThunk(
+    "user/profile",
+    async (user, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await userService.profile(user, token)
+
+        return data
+    }
+)
+
+export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
         resetMessage: (state) => {
             state.message = null
-        }//reseta msg
-    }
+        },//reseta msg
+    },
+    extraReducers: (builder) => {//parte das execuções que são feitas na api//builder-construtor: cria ações separadamente
+        builder.addCase(profile.pending, (state) => {//se a requisição foi enviada mas não obteve nenhuma resposta   
+            state.loading = true
+            state.error = null
+        })
+            .addCase(profile.fulfilled, (state, action) => {//significa que a operação foi concluída com sucesso.
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.user = action.payload//é possível trafegar dados, não apenas mexer nos states exibição na tela
+            })
+    },
 })
-
 export const { resetMessage } = userSlice.actions//função de resetar mensagem
 export default userSlice.reducer//reducer
