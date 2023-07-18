e407b76779401172873972aa8ecc5c50e73b88d8
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-18 16:56:50 -0300

    githubpage + modific.doc

--- reactgram/frontend/src/pages/Auth/Login.js
+++ reactgram/frontend/src/pages/Auth/Login.js
@@ -37,20 +37,18 @@ const Login = () => {
     dispatch(reset())
   }, [dispatch])
 
-  return (
-    <div id="login">
-      <h2>ReactGram</h2>
-      <p className="subtitle">Faça o login para ver o que há de novo.</p>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email} />
-        <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password} />
-        {!loading && <input type="submit" value="Entrar" />}
-        {loading && <input type="submit" disabled value="Aguarde..." />}
-        {error && <Message msg={error} type="error" />}
-      </form>
-      <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
-    </div>
-  )
+  return <div id="login">
+    <h2>ReactGram</h2>
+    <p className="subtitle">Faça o login para ver o que há de novo.</p>
+    {email && <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password} />
+      {!loading && <input type="submit" value="Entrar" />}
+      {loading && <input type="submit" disabled value="Aguarde..." />}
+      {error && <Message msg={error} type="error" />}
+    </form>}
+    <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
+  </div>
 }
 
 export default Login
