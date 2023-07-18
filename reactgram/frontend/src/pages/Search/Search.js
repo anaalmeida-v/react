dfb6e04a24a9b24338d5b54da96b6e85ae2b454f
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-18 09:54:00 -0300

    Funcionalidade de pesquisa e estilos

--- reactgram/frontend/src/pages/Search/Search.js
+++ reactgram/frontend/src/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import './Search.css'
 
 //hooks
 import { useEffect } from 'react'
@@ -12,10 +12,53 @@ import PhotoItem from '../../components/PhotoItem'
 import { Link } from 'react-router-dom'
 
 //redux
+import { searchPhotos, like } from '../../slices/photoSlice'
 
 const Search = () => {
+    const query = useQuery()
+    const search = query.get("q")//a pesquisa em si fica nesse elemento
+
+    const dispatch = useDispatch()
+
+    const resetMessage = useResetComponentMessage(dispatch)//reseta mensagem
+
+    const { user } = useSelector(state => state.auth)//usuário autenticado
+    const { photos, loading } = useSelector(state => state.photo)//photos do state
+
+    //load photos - fotos baseadas na busca
+    useEffect(() => {
+
+        dispatch(searchPhotos(search))
+
+    }, [dispatch, search])//função depende do dispatch e do que vem na variável search
+
+    //like a photo - função de like
+    const handleLike = (photo) => {//inicializando foto nos argumentos
+        dispatch(like(photo._id))//resgatando funcao de like e id da foto
+
+        resetMessage()//limpar msg de sucesso
+    }
+
+    if (loading) {
+        <p>Carregando...    </p>
+    }
+
     return (
-        <div>Search</div>
+        <div id='search'>
+            <h2>Você está buscando por: {search}</h2>
+            {photos && photos.map((photo) => (
+                <div key={photo._id}>{/* id da photo */}
+                    <PhotoItem photo={photo} />
+                    <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+                    <Link className="btn" to={`/photos/${photo._id}`}>Ver mais</Link>
+                </div>
+            ))}
+            {photos && photos.length === 0 && (
+            <h2 className="no-photos">
+                Não foram encontrados resultados para a sua busca...    
+            </h2>
+        )}
+        </div>
     )
 }
 
